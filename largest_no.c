8ea154dbd1b253b37e09bc24a098dd1842c76aa0
praveenkumarpkcse <50762992+praveenkumarpkcse@users.noreply.github.com>
2019-05-20 09:36:10 +0530

    Update largest_no.c

--- largest_no.c
+++ largest_no.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main()
 {
-int no1,no2,no3;
-scanf("%d%d%d",&no1,&no2,&no3);
+int no,no2,no3;
+scanf("%d%d%d",&no,&no2,&no3);
 
-if(no1>no2 &&no1>no3)
+if(no>no2 &&no>no3)
 {
-    printf("%d",no1);
+    printf("%d",no);
 }
 else if(no2>no3)
 {
